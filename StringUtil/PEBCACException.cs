3f00225f623865d9d61d437e6b45a9718548698c
Zoboki Árpád <h671213@stud.u-szeged.hu>
2020-02-04 21:17:06 +0100

    Add new type of exception
    Problem Exists between chair and computer

---
+++ StringUtil/PEBCACException.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StringUtil
+{
+    class PEBCACException : Exception
+    {
+
+        private string errorMessage;
+
+        public PEBCACException(string errorMessage) : base(errorMessage)
+        {
+            this.errorMessage = errorMessage;
+        }
+
+        public string getErrorMessage()
+        {
+            return "Te nagyon hülye vagy, mert: " + errorMessage;
+        }
+    }
+}
